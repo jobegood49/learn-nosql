fbdef8223a47312ba17ccdfca79f7f72d1ae2292
Jonathan Nicolas <jonathan.s.nicolas@gmail.com>
2019-02-21 10:55:47 +0700

    Add get all users functionality from database

--- index.js
+++ index.js
@@ -5,9 +5,44 @@ const port = 8000
 
 app.use(bodyParser.json())
 
+// MONGOOSE
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+mongoose.connect(`${process.env.URL}/${process.env.DB_NAME}`, {
+  useNewUrlParser: true,
+})
+
+// User model => users collection
+const User = mongoose.model('User', {
+  name: String,
+  age: Number,
+  email: String,
+  birthDate: Date,
+})
+
+// Task model => tasks collection
+const Task = mongoose.model('Task', {
+  text: String,
+  completed: Boolean,
+  date: Date,
+})
+
 app.get('/', (req, res) => {
   res.send({
     message: 'Hello World',
+  })
+})
+
+// use async if we want to use await inside the function
+app.get('/users', async (req, res) => {
+  const users = await User.find() // slow process
+  // wait until User.find() is finished using await
+
+  // fast process
+  res.send({
+    message: 'List of all users',
+    users: users,
   })
 })
 
